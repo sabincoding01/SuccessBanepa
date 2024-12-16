e5ec49bd31b6fc06a577f232a3857818e7024bb3
sabincoding01 <168974216+sabincoding01@users.noreply.github.com>
2024-12-16 20:14:32 +0545

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { SlideRight } from '../utility/animation';
 
 const Home = () => {
   return (
-    <div className=' sm: pl-7 pt-0 lg:pl-36 pr-11 pt-9'>
+    <div className=' sm: pl-7 pt-0 lg:pl-36 pr-11 '>
       <div className='container grid grid-cols-1 md:grid-cols-2 min-h-[650px} relative'>
         {/* about abroad study */}
         <div className='flex flex-col justify-center py-14 md:pr-16 xl:pr-40 md:py-0'>
@@ -16,7 +16,7 @@ const Home = () => {
             variants={SlideRight(0.4)}
             initial='hidden'
             animate="visible"
-            className='text-orange-600 uppercase font-semibold mt-2'>Success BCEC Abroad Study</motion.p>
+            className=' mt-0 sm:mt-0 text-orange-600 uppercase font-semibold lg:mt-2 text-2xl font-bold'>Success BCEC Abroad Study</motion.p>
 
             <motion.h1  variants={SlideRight(0.4)}
             initial='hidden'
@@ -34,7 +34,7 @@ const Home = () => {
               {/* Call Us button */}
               <a
                 href='tel:9851033828'
-                className='flex items-center bg-green-600 rounded-full font-semibold text-center hover:bg-green-700 text-black py-2 px-8'
+                className='w-40 flex items-center bg-green-600 rounded-full font-semibold text-center hover:bg-green-700 text-black py-2 px-8 md:w-40'
               >
                 <FaPhoneAlt className='mr-2' /> Call Us
               </a>
@@ -46,7 +46,7 @@ const Home = () => {
           </div>
         </div>
         {/* about abroad study image */}
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center lg: ml-10'>
           <motion.img
             initial={{ opacity: 0, x: 200 }}
             animate={{ opacity: 1, x: 0 }}
@@ -62,3 +62,4 @@ const Home = () => {
 };
 
 export default Home;
+
