e3c638e19d9ae1226f0323b3318264e065ec5409
Sabin <sabintimalsina74@gmail.com>
2024-12-21 20:54:50 +0545

    image

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -6,7 +6,19 @@ const images = [
   '/images/image1.jpeg',
   '/images/image2.jpeg',
   '/images/image3.jpeg',
-  // Add paths for all images
+  '/images/image4.jpeg',
+  '/images/image5.jpeg',
+  '/images/image6.jpeg',
+  '/images/image7.jpeg',
+  '/images/image8.jpeg',
+  '/images/image9.jpeg',
+  '/images/image10.jpeg',
+  '/images/image11.jpeg',
+  '/images/image12.jpeg',
+  '/images/image13.jpeg',
+
+  
+  
 ];
 
 const Gallery = () => {
